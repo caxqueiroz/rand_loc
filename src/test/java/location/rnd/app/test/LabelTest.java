041db545333a6fe43c3eafc0e5b029b49bef4cdc
Carlos Queiroz <caxqueiroz@gmail.com>
2013-03-30 18:06:59 +1100

    tests for Label and LocationType

---
+++ src/test/java/location/rnd/app/test/LabelTest.java
@@ -0,0 +1,49 @@
+package location.rnd.app.test;
+
+import junit.framework.TestCase;
+import location.rnd.app.Label;
+
+public class LabelTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testHashCode() {
+		Label lb = new Label("Melbourne", "en_AU");
+		
+		assertEquals(lb.hashCode(), new Label("Melbourne", "en_AU").hashCode());
+		assertTrue(lb.hashCode()!=new Label("Victoria", "en_AU").hashCode());
+		
+		
+	}
+
+	public void testLabel() {
+		Label lb = new Label("Melbourne", "en_AU");
+		assertNotNull(lb);
+	}
+
+	public void testGetLabel() {
+		Label lb = new Label("Melbourne", "en_AU");
+		assertNotNull(lb.getLabel());
+		String label = lb.getLabel();
+		assertEquals("Melbourne",label);
+	}
+
+	public void testGetLocale() {
+		Label lb = new Label("Melbourne", "en_AU");
+		assertNotNull(lb.getLocale());
+		String locale = lb.getLocale();
+		assertEquals("en_AU",locale);
+	}
+
+	public void testEqualsObject() {
+		Label lb = new Label("Melbourne", "en_AU");
+		assertEquals(lb, new Label("Melbourne", "en_AU"));
+	}
+
+}
