9ddba319b3103ec437c1f64545d608ef43a6787d
Carlos Queiroz <caxqueiroz@gmail.com>
2013-03-31 15:15:13 +1100

    locationontology tests added.

---
+++ src/test/java/location/rnd/app/test/LocationOntologyTest.java
@@ -0,0 +1,31 @@
+package location.rnd.app.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocationOntologyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+
+	@Test
+	public void testAddLocationType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+}
